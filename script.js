035e92c64ce48a01e394fff6ea3de147d46e2c93
Tobias Dodge <tobydodge65@gmail.com>
2024-05-15 21:21:12 -0230

    Using LocalStorage to get persistance between sessions

--- script.js
+++ script.js
@@ -3,20 +3,55 @@ const weatherType = document.querySelector('#weatherType')
 const temperature = document.querySelector('#temperature')
 const humidity = document.querySelector('#humidity')
 const description = document.querySelector('#description')
-const submit = document.querySelector('#submit')
+const submit = document.querySelector('#submitBtn')
 const results = document.querySelector('.infoarea')
 
-function getWeather(postalCode) {
-
+async function getCredentials() {
+    const response = await fetch('/credentials.json');
+    const data = await response.json();
+    return data;
 }
 
-function getPastes() {
-    // fetch pastes from the API
+function addToLocalStorage(key, data) {
+    var local = localStorage.getItem(key)
+    
+    if(!local === null) {
+        local += JSON.stringify(data)
+        localStorage.setItem(key, local)
+    }
 
+    localStorage.setItem(key, JSON.stringify(data))
 }
 
-function setPaste(paste) {
-    // send a paste to the API
+function getFromLocalStorage(key) {
+    return JSON.parse(localStorage.getItem(key))
+}
+
+async function getWeather(postalCode) {
+    const res = await axios.get(`http://api.weatherapi.com/v1/current.json?key=${await getCredentials().weather_key}&q=${postalCode}&aqi=no`)
+
+    return res.data;
+}
+
+function getPrevData() {
+    
+}
+
+async function addData(data) {
+    let credentials = await getCredentials()
+
+    const url = "https://pastebin.com/api/api_post.php"
+    const formData = new FormData()
+    formData.append('api_dev_key', credentials.pastebin_key)
+    console.log(credentials.pastebin_key)
+    formData.append('api_user_name', credentials.pastebin_user)
+    formData.append('api_user_password', credentials.pastebin_password)
+    formData.append('api_option', 'paste')
+    formData.append('api_paste_code', JSON.stringify(data))
+
+    const response = await axios.post(url, formData)
+
+    console.log(response.data)
 
 }
 
@@ -24,3 +59,7 @@ function compare(userData, weatherData) {
     // compare data and return a result
 
 }
+
+submit.addEventListener('click', async () => {
+    await addData("Hello World!")
+})
